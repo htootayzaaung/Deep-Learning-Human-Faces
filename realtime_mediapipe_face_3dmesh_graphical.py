8cfdf07d5afb504f9f4347eda4586aaeeffe08e0
htootayzaaung <htootayzaaung.01@gmail.com>
2023-12-20 20:33:00 +0000

    Finalizing all face detection algorithms.

--- realtime_mediapipe_face_3dmesh_graphical.py
+++ realtime_mediapipe_face_3dmesh_graphical.py
@@ -1,14 +1,16 @@
 import mediapipe as mp
 import cv2
-from PIL import Image, ImageDraw
 import numpy as np
+from PIL import Image, ImageDraw
 
 # Initialize MediaPipe Face Mesh.
 mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh()
-
-# Define drawing specifications for the mesh
-drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=1, circle_radius=1)
+face_mesh = mp_face_mesh.FaceMesh(
+    max_num_faces=5,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
 
 # Start capturing video from the default camera.
 webcam_video_stream = cv2.VideoCapture(0)
@@ -29,21 +31,34 @@ while True:
 
     # Overlay landmarks on the RGB frame.
     if results.multi_face_landmarks:
-        for face_landmarks in results.multi_face_landmarks:
-            # Convert the MediaPipe landmark data to a PIL image
-            pil_image = Image.fromarray(current_frame_rgb)
-            draw = ImageDraw.Draw(pil_image)
-            
-            # Draw the facial landmarks and mesh edges on the frame
-            mp.solutions.drawing_utils.draw_landmarks(
-                image=current_frame_rgb,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec=drawing_spec,
-                connection_drawing_spec=drawing_spec)
+        # Convert the MediaPipe landmark data to a PIL image
+        pil_image = Image.fromarray(current_frame_rgb)
+        draw = ImageDraw.Draw(pil_image)
 
-            # Convert the PIL image back to an array and replace the current frame
-            current_frame = np.array(pil_image)
+        for face_landmarks in results.multi_face_landmarks:
+            # Draw the points
+            for landmark in face_landmarks.landmark:
+                x = int(landmark.x * w)
+                y = int(landmark.y * h)
+                draw.point((x, y), 'green')
+
+            # Draw the mesh lines
+            for connection in mp_face_mesh.FACEMESH_TESSELATION:
+                start_idx = connection[0]
+                end_idx = connection[1]
+                start_point = face_landmarks.landmark[start_idx]
+                end_point = face_landmarks.landmark[end_idx]
+                start_point = (int(start_point.x * w), int(start_point.y * h))
+                end_point = (int(end_point.x * w), int(end_point.y * h))
+                draw.line([start_point, end_point], fill='green', width=1)
+
+        # Convert the PIL image back to an RGB array
+        current_frame_rgb_array = np.array(pil_image)
+        # Convert the RGB array back to BGR, which OpenCV expects
+        current_frame = cv2.cvtColor(current_frame_rgb_array, cv2.COLOR_RGB2BGR)
+    else:
+        # If no landmarks are detected, just display the original frame
+        current_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2BGR)
 
     # Display the frame with landmarks.
     cv2.imshow("Webcam Facial Landmarks", current_frame)
---
+++ video_face_detection_single_shot(SSD).py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+# Capture the video from the default webcam
+webcam_video_stream = cv2.VideoCapture(0)  # Changed to '0' for default webcam
+
+face_detection_classifier = cv2.dnn.readNetFromCaffe('Models/deploy.prototxt', 'Models/res10_300x300_ssd_iter_140000.caffemodel')
+
+# Loop through every frame in the video
+while True:
+    # Get the current frame from the video stream as an image
+    ret, current_frame = webcam_video_stream.read()
+    if not ret:
+        break
+
+    img_height, img_width = current_frame.shape[:2]
+
+    resized_image = cv2.resize(current_frame, (300, 300))
+
+    image_to_detect_blob = cv2.dnn.blobFromImage(resized_image, 1.0, (300, 300), (104, 177, 123))
+
+    face_detection_classifier.setInput(image_to_detect_blob)
+
+    all_face_locations = face_detection_classifier.forward()
+
+    no_of_detections = all_face_locations.shape[2]
+
+    for index in range(no_of_detections):
+        detection_confidence = all_face_locations[0, 0, index, 2]
+        
+        if detection_confidence > 0.5:
+            current_face_location = all_face_locations[0, 0, index, 3:7] * np.array([img_height, img_width, img_width, img_height])
+            left_x, left_y, right_x, right_y = current_face_location.astype("int")
+
+            print('Found face {} at left_x:{}, left_y:{}, right_x:{}, right_y:{}'.format(index+1, left_x, left_y, right_x, right_y))
+            cv2.rectangle(current_frame, (left_x, left_y), (right_x, right_y), (0, 255, 0), 2)
+
+    cv2.imshow("Web-Cam Video", current_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the resources
+webcam_video_stream.release()
+cv2.destroyAllWindows()
